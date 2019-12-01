1292d35b70fc61221f676237d6cc5934f762bcca
einar.hjorleifsson <einar.hjorleifsson@gmail.com>
2019-12-01 08:50:47 +0000

    state as of 2019_12_01

--- R/update_homepage.R
+++ R/update_homepage.R
@@ -1,4 +1,9 @@
 # copy content to webpage
+system("cp ~/edu/spatialr/*.html /net/www/export/home/hafri/einarhj/public_html/spatialr/.")
+system("cp ~/edu/spatialr/*.Rmd /net/www/export/home/hafri/einarhj/public_html/spatialr/.")
+system("chmod -R a+rx /net/www/export/home/hafri/einarhj/public_html/spatialr")
+
+# knit the whole stuff - use only if needed
 library(rmarkdown)
 fil <- c(dir(".", pattern = "*.Rmd"), dir(".", pattern = "*.rmd"))
 fil <- fil[grep("pre_*", fil)]
@@ -6,6 +11,3 @@ for(i in 1:length(fil)) {
   print(fil[i])
   render(input = fil[i])
 }
-system("cp ~/edu/spatialr/*.html /net/www/export/home/hafri/einarhj/public_html/spatialr/.")
-system("cp ~/edu/spatialr/*.Rmd /net/www/export/home/hafri/einarhj/public_html/spatialr/.")
-system("chmod -R a+rx /net/www/export/home/hafri/einarhj/public_html/spatialr")
