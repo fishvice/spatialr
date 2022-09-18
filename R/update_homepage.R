1b794f20cbb28f4b44f6b21ba87dfddffc4cc222
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2022-09-18 10:40:59 +0000

    bugfixes

--- R/update_homepage.R
+++ R/update_homepage.R
@@ -1,7 +1,7 @@
 # copy content to webpage
-system("cp ~/edu/spatialr/*.html /net/www/export/home/hafri/einarhj/public_html/spatialr/.")
-system("cp ~/edu/spatialr/*.Rmd /net/www/export/home/hafri/einarhj/public_html/spatialr/.")
-system("chmod -R a+rx /net/www/export/home/hafri/einarhj/public_html/spatialr")
+system("cp ~/edu/spatialr/*.html /net/www/export/home/hafri/einarhj/public_html/older/spatialr/.")
+system("cp ~/edu/spatialr/*.Rmd /net/www/export/home/hafri/einarhj/public_html/older/spatialr/.")
+system("chmod -R a+rx /net/www/export/home/hafri/einarhj/public_html/older/spatialr")
 
 # knit the whole stuff - use only if needed
 library(rmarkdown)
